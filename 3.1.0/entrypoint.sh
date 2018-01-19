c1e955f011806af794afb605eb1bda09d6d77520
Maximilian Münchow <maximilian.muenchow@teamspeak.com>
2018-01-19 11:40:59 +0100

    Changed config prefix from TEAMSPEAK to TS3SERVER

--- 3.1.0/entrypoint.sh
+++ 3.1.0/entrypoint.sh
@@ -38,37 +38,37 @@ file_env() {
 }
 
 if [ "$1" = 'ts3server' ]; then
-	file_env 'TEAMSPEAK_DB_HOST'
-	file_env 'TEAMSPEAK_DB_USER'
-	file_env 'TEAMSPEAK_DB_PASSWORD'
-	file_env 'TEAMSPEAK_DB_NAME'
+	file_env 'TS3SERVER_DB_HOST'
+	file_env 'TS3SERVER_DB_USER'
+	file_env 'TS3SERVER_DB_PASSWORD'
+	file_env 'TS3SERVER_DB_NAME'
 	
 	if [ ! -f '/opt/ts3server/ts3server.ini' ]; then
 		cat <<- EOF >/opt/ts3server/ts3server.ini
-			licensepath=${TEAMSPEAK_LICENSEPATH}
-			query_ip_whitelist=${TEAMSPEAK_IP_WHITELIST:-query_ip_whitelist.txt}
-			query_ip_blacklist=${TEAMSPEAK_IP_BLACKLIST:-query_ip_blacklist.txt}
-			dbplugin=${TEAMSPEAK_DB_PLUGIN:-ts3db_sqlite3}
-			dbpluginparameter=${TEAMSPEAK_DB_PLUGINPARAMETER:-/opt/ts3server/ts3db.ini}
-			dbsqlpath=${TEAMSPEAK_DB_SQLPATH:-/opt/ts3server/sql/}
-			dbsqlcreatepath=${TEAMSPEAK_DB_SQLCREATEPATH:-create_sqlite}
-			dbconnections=${TEAMSPEAK_DB_CONNECTIONS:-10}
-			dbclientkeepdays=${TEAMSPEAK_DB_CLIENTKEEPDAYS:-30}
-			logpath=${TEAMSPEAK_LOG_PATH:-/var/ts3server/logs}
-			logquerycommands=${TEAMSPEAK_LOG_QUERY_COMMANDS:-0}
-			logappend=${TEAMSPEAK_LOG_APPEND:-0}
+			licensepath=${TS3SERVER_LICENSEPATH}
+			query_ip_whitelist=${TS3SERVER_IP_WHITELIST:-query_ip_whitelist.txt}
+			query_ip_blacklist=${TS3SERVER_IP_BLACKLIST:-query_ip_blacklist.txt}
+			dbplugin=${TS3SERVER_DB_PLUGIN:-ts3db_sqlite3}
+			dbpluginparameter=${TS3SERVER_DB_PLUGINPARAMETER:-/opt/ts3server/ts3db.ini}
+			dbsqlpath=${TS3SERVER_DB_SQLPATH:-/opt/ts3server/sql/}
+			dbsqlcreatepath=${TS3SERVER_DB_SQLCREATEPATH:-create_sqlite}
+			dbconnections=${TS3SERVER_DB_CONNECTIONS:-10}
+			dbclientkeepdays=${TS3SERVER_DB_CLIENTKEEPDAYS:-30}
+			logpath=${TS3SERVER_LOG_PATH:-/var/ts3server/logs}
+			logquerycommands=${TS3SERVER_LOG_QUERY_COMMANDS:-0}
+			logappend=${TS3SERVER_LOG_APPEND:-0}
 		EOF
 	fi
 	if [ ! -f '/opt/ts3server/ts3db.ini' ]; then
 		cat <<- EOF >/opt/ts3server/ts3db.ini
 			[config]
-			host='${TEAMSPEAK_DB_HOST}'
-			port='${TEAMSPEAK_DB_PORT:-3306}'
-			username='${TEAMSPEAK_DB_USER}'
-			password='${TEAMSPEAK_DB_PASSWORD}'
-			database='${TEAMSPEAK_DB_NAME}'
+			host='${TS3SERVER_DB_HOST}'
+			port='${TS3SERVER_DB_PORT:-3306}'
+			username='${TS3SERVER_DB_USER}'
+			password='${TS3SERVER_DB_PASSWORD}'
+			database='${TS3SERVER_DB_NAME}'
 			socket=
-			wait_until_ready='${TEAMSPEAK_DB_WAITUNTILREADY:-30}'
+			wait_until_ready='${TS3SERVER_DB_WAITUNTILREADY:-30}'
 		EOF
 	fi
 fi
