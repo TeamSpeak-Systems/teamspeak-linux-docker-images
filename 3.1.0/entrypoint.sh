0aa60b817eec11e41f8a93b225bab48f82133b1b
Maximilian Münchow <maximilian.muenchow@teamspeak.com>
2018-01-30 05:15:19 +0100

    removed multistage building, made default uid/gid to be 9987, binary input mode for sha256sum and moved generated config files to /var/run/ts3server/ to better support --user.

--- 3.1.0/entrypoint.sh
+++ 3.1.0/entrypoint.sh
@@ -9,7 +9,7 @@ fi
 
 # have the default inifile as the last parameter
 if [ "$1" = 'ts3server' ]; then
-    set -- "$@" inifile=/opt/ts3server/ts3server.ini
+    set -- "$@" inifile=/var/run/ts3server/ts3server.ini
 fi
 
 # usage: file_env VAR [DEFAULT]
@@ -43,34 +43,30 @@ if [ "$1" = 'ts3server' ]; then
 	file_env 'TS3SERVER_DB_PASSWORD'
 	file_env 'TS3SERVER_DB_NAME'
 	
-	if [ ! -f '/opt/ts3server/ts3server.ini' ]; then
-		cat <<- EOF >/opt/ts3server/ts3server.ini
-			licensepath=${TS3SERVER_LICENSEPATH}
-			query_ip_whitelist=${TS3SERVER_IP_WHITELIST:-query_ip_whitelist.txt}
-			query_ip_blacklist=${TS3SERVER_IP_BLACKLIST:-query_ip_blacklist.txt}
-			dbplugin=${TS3SERVER_DB_PLUGIN:-ts3db_sqlite3}
-			dbpluginparameter=${TS3SERVER_DB_PLUGINPARAMETER:-/opt/ts3server/ts3db.ini}
-			dbsqlpath=${TS3SERVER_DB_SQLPATH:-/opt/ts3server/sql/}
-			dbsqlcreatepath=${TS3SERVER_DB_SQLCREATEPATH:-create_sqlite}
-			dbconnections=${TS3SERVER_DB_CONNECTIONS:-10}
-			dbclientkeepdays=${TS3SERVER_DB_CLIENTKEEPDAYS:-30}
-			logpath=${TS3SERVER_LOG_PATH:-/var/ts3server/logs}
-			logquerycommands=${TS3SERVER_LOG_QUERY_COMMANDS:-0}
-			logappend=${TS3SERVER_LOG_APPEND:-0}
-		EOF
-	fi
-	if [ ! -f '/opt/ts3server/ts3db.ini' ]; then
-		cat <<- EOF >/opt/ts3server/ts3db.ini
-			[config]
-			host='${TS3SERVER_DB_HOST}'
-			port='${TS3SERVER_DB_PORT:-3306}'
-			username='${TS3SERVER_DB_USER}'
-			password='${TS3SERVER_DB_PASSWORD}'
-			database='${TS3SERVER_DB_NAME}'
-			socket=
-			wait_until_ready='${TS3SERVER_DB_WAITUNTILREADY:-30}'
-		EOF
-	fi
+	cat <<- EOF >/var/run/ts3server/ts3server.ini
+		licensepath=${TS3SERVER_LICENSEPATH}
+		query_ip_whitelist=${TS3SERVER_IP_WHITELIST:-query_ip_whitelist.txt}
+		query_ip_blacklist=${TS3SERVER_IP_BLACKLIST:-query_ip_blacklist.txt}
+		dbplugin=${TS3SERVER_DB_PLUGIN:-ts3db_sqlite3}
+		dbpluginparameter=${TS3SERVER_DB_PLUGINPARAMETER:-/var/run/ts3server/ts3db.ini}
+		dbsqlpath=${TS3SERVER_DB_SQLPATH:-/opt/ts3server/sql/}
+		dbsqlcreatepath=${TS3SERVER_DB_SQLCREATEPATH:-create_sqlite}
+		dbconnections=${TS3SERVER_DB_CONNECTIONS:-10}
+		dbclientkeepdays=${TS3SERVER_DB_CLIENTKEEPDAYS:-30}
+		logpath=${TS3SERVER_LOG_PATH:-/var/ts3server/logs}
+		logquerycommands=${TS3SERVER_LOG_QUERY_COMMANDS:-0}
+		logappend=${TS3SERVER_LOG_APPEND:-0}
+	EOF
+	cat <<- EOF >/var/run/ts3server/ts3db.ini
+		[config]
+		host='${TS3SERVER_DB_HOST}'
+		port='${TS3SERVER_DB_PORT:-3306}'
+		username='${TS3SERVER_DB_USER}'
+		password='${TS3SERVER_DB_PASSWORD}'
+		database='${TS3SERVER_DB_NAME}'
+		socket=
+		wait_until_ready='${TS3SERVER_DB_WAITUNTILREADY:-30}'
+	EOF
 fi
 
 exec "$@"
