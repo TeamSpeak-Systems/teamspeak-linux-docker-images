d9b88b27481a61c4a9c97a3338c6a71baa783333
Alex <hroostik@gmail.com>
2023-02-10 14:53:07 +0100

    ENV var for create_default_virtualserver

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -76,6 +76,7 @@ if [ "$1" = 'ts3server' ]; then
         ${TS3SERVER_MACHINE_ID:+machine_id=${TS3SERVER_MACHINE_ID}}
         ${TS3SERVER_QUERY_SKIPBRUTEFORCECHECK:+query_skipbruteforcecheck=${TS3SERVER_QUERY_SKIPBRUTEFORCECHECk}}
         ${TS3SERVER_HINTS_ENABLED:+hints_enabled=${TS3SERVER_HINTS_ENABLED}}
+        create_default_virtualserver=${TS3SERVER_CREATE_DEFAULT_VIRTUALSERVER:-1}
 EOF
 
     cat << EOF | sed 's/^[ \t]*//;s/[ \t]*$//;/^$/d' > /var/run/ts3server/ts3db.ini
